b78bd4a74a4d2a03cbf69c87ebcc9e98b7dc3632
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 20:12:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

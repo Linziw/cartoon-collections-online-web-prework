8ea1b698350b1fce7b533b68085b5f0c6d62fd6f
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 20:21:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 array = ["ham", "cheddar", "beef"] 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.
+  
 end

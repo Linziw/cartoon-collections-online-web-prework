3d6bd3681ca2aed46a6358bec37c689f94c72e30
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-27 20:34:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |food|
    cheese_types.include?(food)
-      food
+      return food
   end
 end
 end
